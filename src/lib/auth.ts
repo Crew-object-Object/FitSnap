62190d25111d144d7d26d2ebb2a892c33346addf
Jay Kerkar <bytewise0405@gmail.com>
2025-02-22 06:36:22 +0000

    OAuth Setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import prisma from "./prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://reimagined-tribble-jxg6q95xpp9c4x-3000.app.github.dev/api/auth",
+  ],
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
