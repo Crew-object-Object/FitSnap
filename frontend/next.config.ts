037255be2cda1c98ab99f8a8dd3a28f333b15055
Jay Kerkar <bytewise0405@gmail.com>
2025-02-22 12:23:00 +0000

    Added Leaderboard Section

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import withSerwistInit from "@serwist/next";
-import { url } from "inspector";
 
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
