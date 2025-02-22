5d8560dec677bf6f01faf13b14c61f8ade10b314
sundaram123krishnan <krishnsundaram@gmail.com>
2025-02-22 13:30:02 +0530

    fix typography

--- frontend/src/components/typography/H2.tsx
+++ frontend/src/components/typography/H2.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-export function TypographyH2(children: ReactNode) {
+export function TypographyH2({ children }: { children: ReactNode }) {
   return (
     <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
       {children}
